0df5ed44d9cdd3b2d3944a4dce9bf5db83c3b362
Andres Callanaupa <avc278@nyu.edu>
2020-03-14 09:45:20 -0400

    added everyNTimes()

--- code-stretches-blank.js
+++ code-stretches-blank.js
@@ -763,3 +763,35 @@ no again
 */
 
 /* ------------------------------------------------------------------------------------------------------------------ */
+
+const everyNTimes = () => {
+
+};
+
+const everyOther = everyNTimes(2);
+everyOther('a').catch(r => console.log(`NO ${r}`));
+everyOther('b').then(r => console.log(`YES ${r}`));
+everyOther('c').catch(r => console.log(`NO ${r}`));
+everyOther('d').then(r => console.log(`YES ${r}`));
+
+const everyThree = everyNTimes(3);
+everyThree('1').catch(r => console.log(`NO ${r}`));
+everyThree('2').catch(r => console.log(`NO ${r}`));
+everyThree('3').then(r => console.log(`YES ${r}`));
+everyThree('4').catch(r => console.log(`NO ${r}`));
+everyThree('5').catch(r => console.log(`NO ${r}`));
+everyThree('6').then(r => console.log(`YES ${r}`));
+/*
+NO a
+YES b
+NO c
+YES d
+NO 1
+NO 2
+YES 3
+NO 4
+NO 5
+YES 6
+*/
+
+/* ------------------------------------------------------------------------------------------------------------------ */
