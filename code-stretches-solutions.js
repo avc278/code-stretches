476bb1d1cf19d6f0217495b6e9e4b2d1cd7a39cd
Andres Callanaupa <avc278@nyu.edu>
2020-03-24 16:47:24 -0400

    added Slack unsubscribe

--- code-stretches-solutions.js
+++ code-stretches-solutions.js
@@ -1422,3 +1422,52 @@ FSA - #general - hello there again!!
 */
 
 /* ------------------------------------------------------------------------------------------------------------------ */
+
+// Slack Unsubscribe
+// Fix the sub method so that a listener can unsubscribe
+class Slack{
+    constructor({ workspace }){
+        this.workspace = workspace;
+        this.listeners = {};
+    }
+    getChannel(type){
+        this.listeners[type] = this.listeners[type] || [];
+        return this.listeners[type];
+    }
+    sub(type, fn){
+        this.getChannel(type).push(fn);
+        return () => {
+            this.listeners[type] = this.listeners[type].filter(listener => listener !== fn)
+        }
+    }
+    pub(type, msg){
+        this.getChannel(type).forEach( listener => listener(`${this.workspace } - ${type} - ${msg}`))
+    }
+  }
+const fsa = new Slack({ workspace: 'FSA'});
+const google = new Slack({ workspace: 'Google'});
+
+google.sub('#jokes', (message)=> console.log(message));
+
+google.pub('#jokes', 'What do you call a grandfather clock? An old timer!')
+
+fsa.sub('2001-FLEX', (message)=> {
+    console.log(message)
+});
+
+const unsubscribe = fsa.sub('#general', (message)=> {
+    console.log(message)
+});
+
+fsa.pub('2001-FLEX', 'stretch is open');
+fsa.pub('#general', 'hello there!');
+unsubscribe();
+fsa.pub('#general', 'hello there again!!');
+  
+/*
+Google - #jokes - What do you call a grandfather clock? An old timer!
+FSA - 2001-FLEX - stretch is open
+FSA - #general - hello there!
+*/
+
+/* ------------------------------------------------------------------------------------------------------------------ */
